27aff81e743f497646d30b51640a0240e4f9503a
Helen Hood <helen.c.hood@gmail.com>
2015-02-15 18:33:41 -0500

    Comments displayed dynamically

--- public/scripts/tutorial.js
+++ public/scripts/tutorial.js
@@ -1,3 +1,8 @@
+var data = [
+  {author: "Pete Hunt", text: "This is one comment"},
+  {author: "Jordan Walke", text: "This is *another* comment"}
+];
+
 var converter = new Showdown.converter();
 
 var CommentBox = React.createClass({
@@ -5,7 +10,7 @@ var CommentBox = React.createClass({
     return (
       <div className="commentBox">
         <h1>Comments</h1>
-        <CommentList />
+        <CommentList data={this.props.data} />
         <CommentForm />
       </div>
     );
@@ -14,10 +19,17 @@ var CommentBox = React.createClass({
 
 var CommentList = React.createClass({
   render: function() {
+    var commentNodes = this.props.data.map(function(comment) {
+      return (
+        <Comment author={comment.author}>
+          {comment.text}
+        </Comment>
+      );
+    });
+
     return (
       <div className="commentList">
-        <Comment author="Pete Hunt">This is one comment.</Comment>
-        <Comment author="Helen Hood">This is *another* comment.</Comment>
+        {commentNodes}
       </div>
     );
   }
@@ -48,6 +60,6 @@ var Comment = React.createClass({
 });
 
 React.render(
-  <CommentBox />,
+  <CommentBox data={data} />,
   document.getElementById('content')
 );
